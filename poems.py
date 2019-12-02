498bb8cfdd2180346fc96deaafd7512057d343ea
Simonharris3 <sharris4@oberlin.edu>
2019-12-02 17:47:20 -0500

    Update poems.py

--- poems.py
+++ poems.py
@@ -5,6 +5,13 @@ import re
 
 def read_from_csv(file):
     reader = csv.reader(file, delimiter=",")
+    labels = []
+    poems = []
+    for row in reader:
+        poems.append(row[0])
+        labels.append(row[1])
+
+    return poems, labels
 
 # Returns a list of all poems in the format of a list of stanzas
 # Pass in the list of poems
@@ -35,19 +42,13 @@ def word_diversity(poem):
             unique_words.append(word)
     return len(unique_words)/len(poem)
 
-def get_length():
-    labels = []
-    poems = []
-    for row in reader:
-        poems.append(row[0])
-        labels.append(row[1])
-
-    return poems, labels
-
-
 def pos_counts(poems):
     pos_counts = []
     for poem in poems:
         tagged_poem = nltk.pos_tag(poem)
-        pos_counts.append(
-            {'ADJ': 0, 'ADP': 0, 'ADV': 0, 'CONJ': 0, 'DET': 0, 'NOUN': 0, 'NUM': 0, 'NUM': 0, 'PRON': 0, 'VERB': 0})
+        pos_count = \
+            {'ADJ': 0, 'ADP': 0, 'ADV': 0, 'CONJ': 0, 'DET': 0, 'NOUN': 0, 'NUM': 0, 'NUM': 0, 'PRON': 0, 'VERB': 0}
+
+        
+        pos_counts.append(pos_count)
+
