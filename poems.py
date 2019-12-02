45c93ed9a6cb525f0d9951cee81575fcc6003939
Simonharris3 <sharris4@oberlin.edu>
2019-12-02 17:44:41 -0500

    Update poems.py

--- poems.py
+++ poems.py
@@ -1,6 +1,7 @@
 import nltk
 import csv
 
+
 def read_from_csv(file):
     reader = csv.reader(file, delimiter=",")
 
@@ -12,4 +13,10 @@ def read_from_csv(file):
 
     return poems, labels
 
+
 def pos_counts(poems):
+    pos_counts = []
+    for poem in poems:
+        tagged_poem = nltk.pos_tag(poem)
+        pos_counts.append(
+            {'ADJ': 0, 'ADP': 0, 'ADV': 0, 'CONJ': 0, 'DET': 0, 'NOUN': 0, 'NUM': 0, 'NUM': 0, 'PRON': 0, 'VERB': 0})
